8343ab4aae6a8fb24798bab746d3b007c32be9fd
Cedrik Hoffmann <cedrik.hoffmann@stud.hs-flensburg.de>
2021-12-02 16:08:58 +0100

    Slove Day 01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,31 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var count = 0
+        input.forEachIndexed { index, element ->
+             if (index > 0 && element.toInt() > input[index - 1].toInt()) {
+                count++
+             }
+        }
+        return count
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        val sum = arrayListOf<Int>()
+        var count = 0
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+        input.forEachIndexed { index, _ ->
+            if(input.size >= index + 3) {
+                sum.add(input[index].toInt() + input[index + 1].toInt() + input[index + 2].toInt())
+            }
+        }
+
+        sum.forEachIndexed { index, element ->
+            if (index > 0 && element > sum[index - 1]) {
+                count++
+            }
+        }
+        return count
+    }
 
     val input = readInput("Day01")
     println(part1(input))
